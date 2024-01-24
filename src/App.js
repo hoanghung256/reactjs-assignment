c27ed0eda1af9e1818591413757de96f780f8763
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-01-24 21:40:53 +0700

    Modify App's layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { STAFFS } from './data/staffs';
+import StaffList from './component/staffList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <strong>Staff Management Application</strong>
       </header>
-      <staffList staffs={STAFFS}></staffList>
+
+      <body>
+        <StaffList staffs={STAFFS}></StaffList>
+      </body>
     </div>
   );
 }

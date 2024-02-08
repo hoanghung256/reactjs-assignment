83125de6f554c263731d366ada0d51cb56ac8a5a
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-02-08 15:25:37 +0700

    Update function search staff

--- src/component/StaffList.jsx
+++ src/component/StaffList.jsx
@@ -4,35 +4,38 @@ import { Link } from "react-router-dom";
 
 function StaffList({ api, setStaffDetail }) {
   const [staffList, setStaffList] = useState([]);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
+  const [searchList, setSearchList] = useState([]);
 
   useEffect(() => {
     axios
       .get(api)
       .then((res) => {
-        setStaffList(res.data)
+        setStaffList(res.data);
       })
       .catch((err) => {
         console.log("Error occurred: ", err);
       });
   }, []);
 
-  // function searchStaff() {
-  //   setStaffList(
-  //     staffList.filter((staff) => {
-  //       if (search.toLowerCase() === '') {
-  //         // Handle empty search box case
-  //         return staff;
-  //       } else if (staff.name.toLowerCase().includes(search.toLowerCase())) {
-  //         return staff;
-  //       }
-  //     })
-  //   );
-  // }
+  function searchStaff() {
+    setSearchList(
+      staffList.filter((staff) => {
+        if (search.toLowerCase() === "") {
+          // Handle empty search box case
+          return staff;
+        } else if (staff.name.toLowerCase().includes(search.toLowerCase())) {
+          return staff;
+        }
+      })
+    );
+  }
+
+  const displayList = searchList.length > 0 ? searchList : staffList;
 
   return (
     <div>
-      {/* <div className="search-box d-flex justify-content-end mt-3">
+      <div className="search-box d-flex justify-content-end mt-3">
         <input
           placeholder="Search staffs"
           type="text"
@@ -40,18 +43,20 @@ function StaffList({ api, setStaffDetail }) {
           onChange={(event) => setSearch(event.target.value)}
           onKeyDown={(event) => {
             if (event.key === "Enter") {
-              searchStaff()
+              searchStaff();
             }
           }}
         />
-        <button className="btn btn-primary" onClick={() => searchStaff()}>Search</button>
-      </div> */}
+        <button className="btn btn-primary" onClick={() => searchStaff()}>
+          Search
+        </button>
+      </div>
       <hr />
       <div className="staff-list mx-3">
         <div className="row">
-          {staffList &&
-            staffList.length > 0 &&
-            staffList.map((staff) => (
+          {displayList &&
+            displayList.length > 0 &&
+            displayList.map((staff) => (
               <div key={staff.id} className="wrapper col-6 col-md-4 col-lg-2">
                 <div className="staff-detail border border-dark rounded-1 mt-3">
                   <Link
